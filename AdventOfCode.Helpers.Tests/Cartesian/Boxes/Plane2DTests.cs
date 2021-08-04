f493c522c5c7a9d15f439aef694815a2c7a257d7
Josh Hufford <josh@hufford.io>
2021-08-04 18:55:44 -0400

    Add tests

--- AdventOfCode.Helpers.Tests/Cartesian/Boxes/Plane2DTests.cs
+++ AdventOfCode.Helpers.Tests/Cartesian/Boxes/Plane2DTests.cs
@@ -1,3 +1,4 @@
+using System.Numerics;
 using System.Linq;
 using AdventOfCode.Helpers.Cartesian;
 using AdventOfCode.Helpers.Cartesian.Boxes;
@@ -13,7 +14,8 @@ namespace AdventOfCode.Helpers.Tests.Cartesian.Boxes
             var plane = new Plane2D(new(1, 2), new(3, 4));
             var edges = plane.Edges.ToList();
             Assert.Collection(edges,
-                item => {
+                item =>
+                {
                     Assert.Equal(Vector2D.LeftToRight, item.Orientation.XAxis);
                     Assert.Equal(1, item.Orientation.Determinant);
 
@@ -22,7 +24,8 @@ namespace AdventOfCode.Helpers.Tests.Cartesian.Boxes
                     Assert.Equal(2, item.IEnd.X);
                     Assert.Equal(3, item.IEnd.Y);
                 },
-                item => {
+                item =>
+                {
                     Assert.Equal(Vector2D.BottomToTop, item.Orientation.XAxis);
                     Assert.Equal(1, item.Orientation.Determinant);
 
@@ -31,7 +34,8 @@ namespace AdventOfCode.Helpers.Tests.Cartesian.Boxes
                     Assert.Equal(2, item.IEnd.X);
                     Assert.Equal(4, item.IEnd.Y);
                 },
-                item => {
+                item =>
+                {
                     Assert.Equal(Vector2D.RightToLeft, item.Orientation.XAxis);
                     Assert.Equal(1, item.Orientation.Determinant);
 
@@ -40,7 +44,8 @@ namespace AdventOfCode.Helpers.Tests.Cartesian.Boxes
                     Assert.Equal(1, item.IEnd.X);
                     Assert.Equal(4, item.IEnd.Y);
                 },
-                item => {
+                item =>
+                {
                     Assert.Equal(Vector2D.TopToBottom, item.Orientation.XAxis);
                     Assert.Equal(1, item.Orientation.Determinant);
 
@@ -48,6 +53,104 @@ namespace AdventOfCode.Helpers.Tests.Cartesian.Boxes
                     Assert.Equal(4, item.IStart.Y);
                     Assert.Equal(1, item.IEnd.X);
                     Assert.Equal(3, item.IEnd.Y);
+                }
+            );
+        }
+
+        [Fact]
+        public void EdgesReoriented()
+        {
+            var plane = new Plane2D(new(1, 2), new(3, 4)) { Orientation = new Orientation2D(Vector2D.TopToBottom, Vector2D.LeftToRight) };
+            var edges = plane.Edges.ToList();
+            Assert.Collection(edges,
+                item =>
+                {
+                    Assert.Equal(Vector2D.TopToBottom, item.Orientation.XAxis);
+                    Assert.Equal(1, item.Orientation.Determinant);
+
+                    Assert.Equal(3, item.IStart.X);
+                    Assert.Equal(-1, item.IStart.Y);
+                    Assert.Equal(3, item.IEnd.X);
+                    Assert.Equal(-2, item.IEnd.Y);
+                },
+                item =>
+                {
+                    Assert.Equal(Vector2D.LeftToRight, item.Orientation.XAxis);
+                    Assert.Equal(1, item.Orientation.Determinant);
+
+                    Assert.Equal(3, item.IStart.X);
+                    Assert.Equal(-2, item.IStart.Y);
+                    Assert.Equal(4, item.IEnd.X);
+                    Assert.Equal(-2, item.IEnd.Y);
+                },
+                item =>
+                {
+                    Assert.Equal(Vector2D.BottomToTop, item.Orientation.XAxis);
+                    Assert.Equal(1, item.Orientation.Determinant);
+
+                    Assert.Equal(4, item.IStart.X);
+                    Assert.Equal(-2, item.IStart.Y);
+                    Assert.Equal(4, item.IEnd.X);
+                    Assert.Equal(-1, item.IEnd.Y);
+                },
+                item =>
+                {
+                    Assert.Equal(Vector2D.RightToLeft, item.Orientation.XAxis);
+                    Assert.Equal(1, item.Orientation.Determinant);
+
+                    Assert.Equal(4, item.IStart.X);
+                    Assert.Equal(-1, item.IStart.Y);
+                    Assert.Equal(3, item.IEnd.X);
+                    Assert.Equal(-1, item.IEnd.Y);
+                }
+            );
+        }
+
+        [Fact]
+        public void EdgesFlipped()
+        {
+            var plane = new Plane2D(new(1, 2), new(3, 4)) { Orientation = new Orientation2D(Vector2D.LeftToRight, Vector2D.TopToBottom) };
+            var edges = plane.Edges.ToList();
+            Assert.Collection(edges,
+                item =>
+                {
+                    Assert.Equal(Vector2D.LeftToRight, item.Orientation.XAxis);
+                    Assert.Equal(-1, item.Orientation.Determinant);
+
+                    Assert.Equal(1, item.IStart.X);
+                    Assert.Equal(-3, item.IStart.Y);
+                    Assert.Equal(2, item.IEnd.X);
+                    Assert.Equal(-3, item.IEnd.Y);
+                },
+                item =>
+                {
+                    Assert.Equal(Vector2D.TopToBottom, item.Orientation.XAxis);
+                    Assert.Equal(-1, item.Orientation.Determinant);
+
+                    Assert.Equal(2, item.IStart.X);
+                    Assert.Equal(-3, item.IStart.Y);
+                    Assert.Equal(2, item.IEnd.X);
+                    Assert.Equal(-4, item.IEnd.Y);
+                },
+                item =>
+                {
+                    Assert.Equal(Vector2D.RightToLeft, item.Orientation.XAxis);
+                    Assert.Equal(-1, item.Orientation.Determinant);
+
+                    Assert.Equal(2, item.IStart.X);
+                    Assert.Equal(-4, item.IStart.Y);
+                    Assert.Equal(1, item.IEnd.X);
+                    Assert.Equal(-4, item.IEnd.Y);
+                },
+                item =>
+                {
+                    Assert.Equal(Vector2D.BottomToTop, item.Orientation.XAxis);
+                    Assert.Equal(-1, item.Orientation.Determinant);
+
+                    Assert.Equal(1, item.IStart.X);
+                    Assert.Equal(-4, item.IStart.Y);
+                    Assert.Equal(1, item.IEnd.X);
+                    Assert.Equal(-3, item.IEnd.Y);
                 }
             );
         }
