9b8df7b08d5ae80a2bcb9adb383e45f51324ccf0
Josh Hufford <josh@hufford.io>
2021-08-04 20:37:34 -0400

    Implement faces in Space3D

--- AdventOfCode.Helpers/Cartesian/Boxes/Space3D.cs
+++ AdventOfCode.Helpers/Cartesian/Boxes/Space3D.cs
@@ -6,12 +6,12 @@ namespace AdventOfCode.Helpers.Cartesian.Boxes
     {
         public Orientation3D Orientation { get; init; } = Orientation3D.Standard;
 
-        public Plane3D IStart => throw new NotImplementedException();
-        public Plane3D IEnd => throw new NotImplementedException();
-        public Plane3D JStart => throw new NotImplementedException();
-        public Plane3D JEnd => throw new NotImplementedException();
-        public Plane3D KStart => throw new NotImplementedException();
-        public Plane3D KEnd => throw new NotImplementedException();
+        public Plane3D IStart => new(J, K, I.Start) { Orientation =  new Orientation3D(Vector3D.BottomToTop, Vector3D.FarToNear, Vector3D.LeftToRight) * Orientation };
+        public Plane3D IEnd => new(-J, -K, -I.End) { Orientation =  new Orientation3D(Vector3D.TopToBottom, Vector3D.NearToFar, Vector3D.RightToLeft) * Orientation };
+        public Plane3D JStart => new(K, I, J.Start) { Orientation =  new Orientation3D(Vector3D.FarToNear, Vector3D.LeftToRight, Vector3D.BottomToTop) * Orientation };
+        public Plane3D JEnd => new(-K, -I, -J.Start) { Orientation =  new Orientation3D(Vector3D.NearToFar, Vector3D.RightToLeft, Vector3D.TopToBottom) * Orientation };
+        public Plane3D KStart => new(I, J, K.Start) { Orientation = Orientation3D.Standard * Orientation };
+        public Plane3D KEnd =>  new(-I, -J, -K.Start) { Orientation = -Orientation3D.Standard * Orientation };
 
         public Plane3D[] Faces => new []
         {
