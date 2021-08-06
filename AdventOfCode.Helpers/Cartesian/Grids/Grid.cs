dbeb0df17766c821478b3d57e3a793818ea69240
Josh Hufford <josh@hufford.io>
2021-08-05 22:17:11 -0400

    Simplify help method names

--- AdventOfCode.Helpers/Cartesian/Grids/Grid.cs
+++ AdventOfCode.Helpers/Cartesian/Grids/Grid.cs
@@ -6,8 +6,8 @@ namespace AdventOfCode.Helpers.Cartesian.Grids
         public static SparsePlaneGrid2D<T> MakeSparse<T>(Interval x, Interval y) where T : notnull => new(x, y);
         public static SparseSpaceGrid3D<T> MakeSparse<T>(Interval x, Interval y, Interval z) where T : notnull => new(x, y, z);
 
-        public static SparseLineGrid1D<T> MakeInfiniteSparse1D<T>() where T : notnull => SparseLineGrid1D<T>.Infinite;
-        public static SparseLineGrid2D<T> MakeInfiniteSparse2D<T>() where T : notnull => SparseLineGrid2D<T>.Infinite;
-        public static SparseLineGrid3D<T> MakeInfiniteSparse3D<T>() where T : notnull => SparseLineGrid3D<T>.Infinite;
+        public static SparseLineGrid1D<T> Infinite1D<T>() where T : notnull => SparseLineGrid1D<T>.Infinite;
+        public static SparseLineGrid2D<T> Infinite2D<T>() where T : notnull => SparseLineGrid2D<T>.Infinite;
+        public static SparseLineGrid3D<T> Infinite3D<T>() where T : notnull => SparseLineGrid3D<T>.Infinite;
     }
 }
