03afcdf97e925dfc7cd10d6c832efa69597544a8
Josh Hufford <josh@hufford.io>
2021-08-14 18:52:36 -0400

    Implement AtomicExpression

---
+++ Kleene/Expressions/AtomicExpression.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+
+namespace Kleene
+{
+    public class AtomicExpression : Expression
+    {
+        public Expression Expression { get; }
+
+        public AtomicExpression(Expression expression)
+        {
+            Expression = expression;
+        }
+
+        public override IEnumerable<ExpressionResult> Run(ExpressionContext context)
+        {
+            foreach (var result in Expression.Run(context))
+            {
+                yield return result;
+                yield break;
+            }
+        }
+    }
+}
