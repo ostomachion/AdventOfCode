35efb75020f2f9563eb910b92b545783ddc5cdcb
Josh Hufford <josh@hufford.io>
2021-08-14 18:54:34 -0400

    Disallow nested transform expressions

--- Kleene/Expressions/TransformExpression.cs
+++ Kleene/Expressions/TransformExpression.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace Kleene
 {
@@ -15,24 +16,25 @@ namespace Kleene
 
         public override IEnumerable<ExpressionResult> Run(ExpressionContext context)
         {
-            bool consuming = context.Local.Consuming;
-            bool producing = context.Local.Producing;
+            if (!context.Local.Producing || !context.Local.Consuming)
+                throw new InvalidOperationException("Cannot nest transform expressions.");
+                
             context.Local.Producing = false;
             foreach (var inputResult in Input.Run(context))
             {
-                context.Local.Producing = producing;
+                context.Local.Producing = true;
                 context.Local.Consuming = false;
                 foreach (var outputResult in Output.Run(context))
                 {
-                    context.Local.Producing = producing;
-                    context.Local.Consuming = consuming;
+                    context.Local.Producing = true;
+                    context.Local.Consuming = true;
                     yield return new ExpressionResult(inputResult.Input, outputResult.Output);
                     context.Local.Consuming = false;
                 }
                 context.Local.Producing = false;
-                context.Local.Consuming = consuming;
+                context.Local.Consuming = true;
             }
-            context.Local.Producing = producing;
+            context.Local.Producing = true;
         }
     }
 }
