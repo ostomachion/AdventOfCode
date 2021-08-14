0802c8a929681018a8757a84dfa0eb56b2cc5d39
Josh Hufford <josh@hufford.io>
2021-08-14 16:58:21 -0400

    Add remaining class stubs

---
+++ Kleene/Expressions/AnchorExpression.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+namespace Kleene
+{
+    public class AnchorExpression : Expression
+    {
+        public override IEnumerable<ExpressionResult> Run(ExpressionContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
