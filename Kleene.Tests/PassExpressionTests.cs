43b478d5f1d259af1cdc1e7793ac170ff736e43c
Josh Hufford <josh@hufford.io>
2021-08-14 14:53:14 -0400

    Implement PassExpression

---
+++ Kleene.Tests/PassExpressionTests.cs
@@ -0,0 +1,34 @@
+using System;
+using Xunit;
+
+namespace Kleene.Tests
+{
+    public class PassExpressionTests
+    {
+        [Fact]
+        public void Match()
+        {
+            // Given
+            var expression = new PassExpression();
+
+            // When
+            var result = expression.Transform("");
+
+            // Then
+            Assert.Equal("", result);
+        }
+
+        [Fact]
+        public void NonEmpty()
+        {
+            // Given
+            var expression = new PassExpression();
+
+            // When
+            var result = expression.Transform("x");
+
+            // Then
+            Assert.Null(result);
+        }
+    }
+}
---
+++ Kleene/Expressions/PassExpression.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+
+namespace Kleene
+{
+    public class PassExpression : Expression
+    {
+        public override IEnumerable<ExpressionResult> Run(ExpressionContext context)
+        {
+            yield return new ExpressionResult("");
+        }
+    }
+}
