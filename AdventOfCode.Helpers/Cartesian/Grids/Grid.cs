0a276f6dd55851404992c4e963f95337ebb63955
josh@hufford.io <josh@hufford.io>
2021-11-24 18:46:51 -0500

    Fix Grid helper class bugs

--- AdventOfCode.Helpers/Cartesian/Grids/Grid.cs
+++ AdventOfCode.Helpers/Cartesian/Grids/Grid.cs
@@ -7,6 +7,6 @@ public static class Grid
     public static SparseSpaceGrid3D<T> MakeSparse<T>(Interval x, Interval y, Interval z) where T : notnull => new(x, y, z);
 
     public static SparseLineGrid1D<T> Infinite1D<T>() where T : notnull => SparseLineGrid1D<T>.Infinite;
-    public static SparseLineGrid2D<T> Infinite2D<T>() where T : notnull => SparseLineGrid2D<T>.Infinite;
-    public static SparseLineGrid3D<T> Infinite3D<T>() where T : notnull => SparseLineGrid3D<T>.Infinite;
+    public static SparsePlaneGrid2D<T> Infinite2D<T>() where T : notnull => SparsePlaneGrid2D<T>.Infinite;
+    public static SparsePlaneGrid3D<T> Infinite3D<T>() where T : notnull => SparsePlaneGrid3D<T>.Infinite;
 }
