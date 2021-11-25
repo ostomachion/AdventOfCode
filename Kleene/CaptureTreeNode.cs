af3bb2dd8f8e034af77febfc468b5dc865f6d49c
josh@hufford.io <josh@hufford.io>
2021-11-25 11:50:23 -0500

    Setup for parsing

--- Kleene/CaptureTreeNode.cs
+++ Kleene/CaptureTreeNode.cs
@@ -10,6 +10,7 @@ public class CaptureTreeNode
     public IEnumerable<CaptureTreeNode> Children => children;
     public bool IsOpen { get; set; }
     public bool IsFunctionBoundary { get; set; }
+    public bool IsPropertyBoundary => Name != "" && Char.IsUpper(Name[0]);
 
     public CaptureTreeNode? this[CaptureName? name]
     {
@@ -41,5 +42,80 @@ public class CaptureTreeNode
     {
         var node = children.Pop();
         node.Parent = null;
+    }
+
+    public T Parse<T>()
+    {
+        var typeNode = GetTypeAssignmentNode();
+        var type = typeNode is null ? typeof(T) : ResolveTypeName(typeNode.Name);
+
+        object value;
+        if (type.IsEnum)
+        {
+            // TODO:Enum.TryParse<C>(ReadOnlySpan<char>, out C) || (C)Int32.Parse(ReadOnlySpan<char>)
+            throw new NotImplementedException();
+        }
+        else if (type.GetInterfaces().Contains(typeof(IEnumerable<char>)))
+        {
+            // TODO: parse as string
+            throw new NotImplementedException();
+        }
+        else if (type.GetInterfaces().Any(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IEnumerable<>)))
+        {
+            // TODO: parse each value as type, then construct collection (???)
+            throw new NotImplementedException();
+        }
+        else
+        {
+            // 5. Otherwise, parse as C
+            //      - bool Enum.TryParse<C>(ReadOnlySpan<char>, out C)
+            //      - bool TryParse(ReadOnlySpan<char>, out C)
+            //      - bool TryParse(string, out C)
+            //      - C Parse(ReadOnlySpan<char>)
+            //      - C Parse(string)
+            //      - bool C.TryParse<T>(ReadOnlySpan<char>, out C)
+            //      - bool C.TryParse<T>(string, out C)
+            //      - bool C.TryParse(Type, ReadOnlySpan<char>, out C)
+            //      - bool C.TryParse(Type, string, out C)
+            //      - bool C.TryParse(ReadOnlySpan<char>, out C)
+            //      - bool C.TryParse(string, out C)
+            //      - C C.Parse<C>(ReadOnlySpan<char>)
+            //      - C C.Parse<C>(string)
+            //      - C C.Parse(Type, ReadOnlySpan<char>)
+            //      - C C.Parse(Type, string)
+            //      - C C.Parse(ReadOnlySpan<char>)
+            //      - C C.Parse(string)
+            //      - implicit C (ReadOnlySpan<char>)
+            //      - explicit C (ReadOnlySpan<char>)
+            //      - implicit C (string)
+            //      - explicit C (string)
+            //      - new C()
+            throw new NotImplementedException();
+        }
+
+        // 6. Parse and set explicit properties
+
+        // 7. Parse and set captured properties
+
+        return (T)value;
+    }
+
+    public CaptureTreeNode? GetTypeAssignmentNode()
+    {
+        if (Name == "!T")
+            return this;
+
+        foreach (var node in Children.Where(x => !x.IsFunctionBoundary && !x.IsPropertyBoundary))
+        {
+            if (node.GetTypeAssignmentNode() is CaptureTreeNode value)
+                return value;
+        }
+
+        return null;
+    }
+
+    private Type ResolveTypeName(string name)
+    {
+        throw new NotImplementedException();
     }
 }
