ddeedededee1b0431ed36e5da529d336aebf0765
josh@hufford.io <josh@hufford.io>
2021-11-28 13:59:18 -0500

    And using to both StringExtensions methods

--- Kleene/StringExtensions.cs
+++ Kleene/StringExtensions.cs
@@ -10,7 +10,10 @@ namespace Kleene
     {
         public static T Parse<T>(this string input, string pattern)
         {
-            return Expression.Parse(pattern).Parse<T>(input);
+            var expression = Expression.Parse(pattern);
+            if (typeof(T).Namespace is string ns)
+                expression = new ConcatExpression(new UsingExpression(ns), expression);
+            return expression.Parse<T>(input);
         }
 
         public static IEnumerable<T> Parse<T>(this IEnumerable<string> input, string pattern)
