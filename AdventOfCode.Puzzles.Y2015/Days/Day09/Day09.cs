e822ceff7ee43311b2bfcdf864c7f34c222b778c
josh@hufford.io <josh@hufford.io>
2021-11-29 22:04:33 -0500

    Implement day 9 part 1

--- AdventOfCode.Puzzles.Y2015/Days/Day09/Day09.cs
+++ AdventOfCode.Puzzles.Y2015/Days/Day09/Day09.cs
@@ -1,17 +1,42 @@
 namespace AdventOfCode.Puzzles.Y2015.Days.Day09;
 
-public class Day09 : Day
+public class Day10 : Day
 {
     public override Output Part1()
     {
         var parsed = Input.Lines().Parse<Item>(@"\w+:Start ' to ' \w+:End ' = ' \d+:Distance");
         HashSet<string> locations = parsed.SelectMany(x => new[] { x.Start, x.End }).ToHashSet();
-        
-        return AnswerNotFound();
+        long min = long.MaxValue;
+
+        var max = parsed.MaxBy(x => x.Distance)!;
+
+        foreach (var p in locations.Permutations().Select(x => x.ToList()).Where(x => x.First() == max.Start && x.Last() == max.End))
+        {
+            long d = 0;
+            for (var i = 1; i < p.Count; i++)
+            {
+                d += Item.GetDistance(parsed, new[] { p[i], p[i - 1] });
+                if (d > min)
+                    break;
+            }
+            if (d < min)
+                min = d;
+        }
+        return min;
     }
 
     public override Output Part2()
     {
-        throw new NotImplementedException();
+        var parsed = Input.Lines().Parse<Item>(@"\w+:Start ' to ' \w+:End ' = ' \d+:Distance").ToList();
+        HashSet<string> locations = parsed.SelectMany(x => new[] { x.Start, x.End }).ToHashSet();
+
+        long d = 0;
+        while (parsed.Any())
+        {
+            var max = parsed.MaxBy(x => x.Distance)!;
+            parsed.RemoveAll(x => x.Start == max.Start || x.End == max.Start || x.Start == max.End || x.End == max.End);
+            d += max.Distance;
+        }
+        return d;
     }
 }
--- AdventOfCode.Puzzles.Y2015/Days/Day09/Item.cs
+++ AdventOfCode.Puzzles.Y2015/Days/Day09/Item.cs
@@ -1,3 +1,17 @@
 ﻿namespace AdventOfCode.Puzzles.Y2015.Days.Day09;
 
-public record Item(string Start, string End, long Distance);
+public record Item(string Start, string End, long Distance)
+{
+    public static long GetDistance(IEnumerable<Item> items, IEnumerable<string> p)
+    {
+        var path = p.ToArray();
+        if (path.Length == 2)
+        {
+            return items.First(x => x.Start == path[0] && x.End == path[1] || x.Start == path[1] && x.End == path[0]).Distance;
+        }   
+        else
+        {
+            return GetDistance(items, path[0..2]) + GetDistance(items, path[2..]);
+        }
+    }
+}
