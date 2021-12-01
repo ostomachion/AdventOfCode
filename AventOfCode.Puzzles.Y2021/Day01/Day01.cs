690f74caca5b8191b556dd2b55d242790a0909a8
josh@hufford.io <josh@hufford.io>
2021-12-01 00:08:54 -0500

    Day 1 part 1 (technical difficulties...)

--- AventOfCode.Puzzles.Y2021/Day01/Day01.cs
+++ AventOfCode.Puzzles.Y2021/Day01/Day01.cs
@@ -1,14 +1,24 @@
-﻿namespace AdventOfCode.Puzzles.Y2016.Days.Day01;
+﻿namespace AdventOfCode.Puzzles.Y2021.Days.Day01;
 
 public class Day01 : Day
 {
     public override Output Part1()
     {
+        var test = Input.Lines().Select(Int32.Parse);
+        int val = 0;
+        var prev = 9999999;
+        foreach (var line in test)
+        {
+            if (line > prev)
+                val++;
+            prev = line;
+        }
         return AnswerNotFound();
     }
 
     public override Output Part2()
     {
-        return AnswerNotFound();
+        // TODO:
+        throw new NotImplementedException();
     }
 }
