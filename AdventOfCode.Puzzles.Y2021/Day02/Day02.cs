b4a9ec89259de503a7c0182936bef1fb723eac5a
josh@hufford.io <josh@hufford.io>
2021-12-02 12:36:03 -0500

    Day 2 cleanup

--- AdventOfCode.Puzzles.Y2021/Day02/Day02.cs
+++ AdventOfCode.Puzzles.Y2021/Day02/Day02.cs
@@ -1,31 +1,25 @@
 ﻿namespace AdventOfCode.Puzzles.Y2021.Days.Day02;
 
-public record Item(string Dir, int N);
-
 public class Day02 : Day
 {
     public override Output Part1()
     {
-        var value = Input.Lines().Parse<Item>(@"(forward|down|up):Dir ' ' \d+:N");
+        var value = Input.Lines().Parse<(string Dir, int N)>(@"\w+:Dir ' ' \d+:N");
 
         int x = 0;
         int y = 0;
-        int a = 0;
-        foreach (var v in value)
+        foreach (var (dir, n) in value)
         {
-            switch (v.Dir)
+            switch (dir)
             {
                 case "forward":
-                    x += v.N;
-                    y += a * v.N;
+                    x += n;
                     break;
                 case "down":
-                    y += v.N;
-                    a += v.N;
+                    y += n;
                     break;
                 case "up":
-                    y -= v.N;
-                    a -= v.N;
+                    y -= n;
                     break;
             }
         }
@@ -34,24 +28,24 @@ public class Day02 : Day
 
     public override Output Part2()
     {
-        var value = Input.Lines().Parse<Item>(@"(forward|down|up):Dir ' ' \d+:N");
+        var value = Input.Lines().Parse<(string Dir, int N)>(@"\w+:Dir ' ' \d+:N");
 
         int x = 0;
         int y = 0;
         int a = 0;
-        foreach (var v in value)
+        foreach (var (dir, n) in value)
         {
-            switch (v.Dir)
+            switch (dir)
             {
                 case "forward":
-                    x += v.N;
-                    y += a * v.N;
+                    x += n;
+                    y += a * n;
                     break;
                 case "down":
-                    a += v.N;
+                    a += n;
                     break;
                 case "up":
-                    a -= v.N;
+                    a -= n;
                     break;
             }
         }
