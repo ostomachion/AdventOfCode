47f0d3287587efafe02ca75e101a9b0280860a62
josh@hufford.io <josh@hufford.io>
2021-12-03 23:04:43 -0500

    Prepare for day 4

---
+++ AdventOfCode.Puzzles.Y2021/Day04/Day04.cs
@@ -0,0 +1,18 @@
+﻿namespace AdventOfCode.Puzzles.Y2021.Days.Day04;
+
+public class Day04 : Day
+{
+    public override Output Part1()
+    {
+        var value = Input.Lines();
+
+        return AnswerNotFound();
+    }
+
+    public override Output Part2()
+    {
+        var value = Input.Lines();
+
+        return AnswerNotFound();
+    }
+}
--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -1,3 +1,3 @@
 ﻿using AdventOfCode.Puzzles;
 
-new Runner().Print(2021, 3, 2);
+new Runner().Print();
