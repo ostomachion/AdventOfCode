017c442fe7cf04c48177d2b2edd72d0277d5b609
josh@hufford.io <josh@hufford.io>
2021-12-07 12:42:24 -0500

    Implement day 7

--- AdventOfCode.Helpers/Extensions/MathExtenions.cs
+++ AdventOfCode.Helpers/Extensions/MathExtenions.cs
@@ -7,4 +7,7 @@ public static class MathExtenions
     public static bool IsBetween(this long n, long min, long max) => n >= min && n <= max;
 
     public static long Clamp(this long n, long min, long max) => Math.Min(Math.Max(n, min), max);
+
+    public static long Triangle(this long n) => n * (n + 1) / 2;
+    public static int Triangle(this int n) => n * (n + 1) / 2;
 }
---
+++ AdventOfCode.Puzzles.Y2021/Day07/Day07.cs
@@ -0,0 +1,16 @@
+﻿namespace AdventOfCode.Puzzles.Y2021.Days.Day07;
+
+public class Day07 : Day
+{
+    public override Output Part1()
+    {
+        var input = Input.Split(',').Select(Int32.Parse);
+        return Enumerable.Range(0, input.Max()).Select(x => input.Select(y => Math.Abs(x - y)).Sum()).Min();
+    }
+
+    public override Output Part2()
+    {
+        var input = Input.Split(',').Select(Int32.Parse);
+        return Enumerable.Range(0, input.Max()).Select(x => input.Select(y => Math.Abs(x - y).Triangle()).Sum()).Min();
+    }
+}
