2016131c24df6eb20f411d574449385bd5364e2f
josh@hufford.io <josh@hufford.io>
2021-12-08 21:43:05 -0500

    Prepare for day 9

---
+++ AdventOfCode.Puzzles.Y2021/Day09/Day09.cs
@@ -0,0 +1,19 @@
+﻿namespace AdventOfCode.Puzzles.Y2021.Days.Day09;
+
+public class Day09 : Day
+{
+    public override string? TestInput => null;
+
+    public override Output? TestOutputPart1 => null;
+    public override Output? TestOutputPart2 => null;
+
+    public override Output Part1()
+    {
+        return AnswerNotFound();
+    }
+
+    public override Output Part2()
+    {
+        return AnswerNotFound();
+    }
+}
